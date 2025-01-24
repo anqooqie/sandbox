fbdc88d8ab9d3f257a57838fdbef5fe68ae8690f
anqooqie <zzzfuku1991@gmail.com>
2025-01-24 19:52:07 +0900

    Add one verification file

---
+++ tests/hello.test.cpp
@@ -0,0 +1,10 @@
+// competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/aplusb
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    cout << A + B << '\n';
+}
